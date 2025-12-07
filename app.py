ec1a63e8dd7618599bf33d6fbf8bd28a55a47739
eduardourtado <eduardourtado@gmail.com>
2025-12-07 19:18:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai.errors import APIError
 from google.genai.types import Content, Part
-from fpdf2.fpdf2 import FPDF
+from fpdf.fpdf2 import FPDF
 from datetime import datetime
 
 # --- Função de Limpeza de Estado ---
